003816dba8ec6c797e11f7d6dab36f099d032642
PramodRavindu00 <pramod2000.ravindu@gmail.com>
2026-06-25 20:36:39 +0530

    Implement client-side authentication check in AuthenticatedLayout to redirect unauthorized users, replacing the previous AuthenticatedLayoutGuard component.

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,16 +1,25 @@
-import AuthenticatedLayoutGuard from "@/components/AuthenticatedLayoutGuard";
+"use client";
 import AppBar from "@/components/custom/AppBar";
+import { AuthStatus } from "@/lib/constants/constants";
+import { useAuthStore } from "@/store/auth.store";
+import { redirect } from "next/navigation";
 
 const AuthenticatedLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const authStatus = useAuthStore((s) => s.status);
+
+  if (authStatus !== AuthStatus.Authenticated) {
+    redirect("/unauthorized");
+  }
+
   return (
-    <AuthenticatedLayoutGuard>
-      <AppBar/>
+    <>
+      <AppBar />
       {children}
-    </AuthenticatedLayoutGuard>
+    </>
   );
 };
 
